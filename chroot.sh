ff531bbc1756d23588e18f2505490ffbea708b06
Allespro <39953296+Allespro@users.noreply.github.com>
2018-12-24 17:14:17 +0300

    Add files via upload

---
+++ chroot.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+export LINUXROOT=/data/local/mnt
+export TMPDIR=/tmp
+export HOME=/root
+export USER=root
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH
+export TERM=vt100
+
+mount_and_chroot()
+{
+	echo "*************Mounting*************"
+
+	mount -t proc proc ./proc
+	mount -t sysfs sysfs ./sys
+	mount -o bind /dev ./dev 
+	mount -t devpts devpts ./dev/pts
+	chroot ./ /bin/bash
+
+	echo "*************Exiting from chroot*************"
+}
+
+cd /data/local/mnt/
+FILES="$(ls -1)"
+if [ -n "$FILES" ]; then
+	mount_and_chroot
+else
+	echo "*************Mounting system*************"
+	mount -t ext3 -o rw,noatime /dev/block/mmcblk0p1
+	mount_and_chroot
+fi
